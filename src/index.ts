903f7049b786796693898b6f2c25c915a1c94710
Zao Soula <contact@zaosoula.fr>
2020-09-29 13:43:35 +0200

    Add: func say - according to README specs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
+/* say
+  Return: (string)
+    '0' if the input is 0
+    'Fizz' if the input is a multiple of 3
+    'Buzz' if the input is a multiple of 5
+    'Fizzbuzz' if the input is a multiple of 3 and 5
+    else the input as a string
+*/
 
-const say = (input: number) => input.toString(); 
+export const say = (input: number) => {
+
+  if (input == 0) {
+    return '0';
+  }
+
+  if (input % 3 == 0 && input % 5 == 0) {
+    return 'Fizzbuzz';
+  }
+
+  if (input % 3 == 0) {
+    return 'Fizz';
+  }
+
+  if (input % 5 == 0) {
+    return 'Buzz';
+  }
+
+  return input.toString();
+}
+
 const results = Array.from({ length: 101 })
-                     .map((_, index) => `${index} => ${say(index)}`);
+  .map((_, index) => `${index} => ${say(index)}`);
 
 console.log(results.join('\n'));
