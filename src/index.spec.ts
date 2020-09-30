c9fb3f1134baf6a3e129de64f651d671a2bd55fa
Zao Soula <contact@zaosoula.fr>
2020-09-30 08:44:27 +0200

    Fix: simplified unit testing

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -37,27 +37,19 @@ describe('FizzBuzz test', () => {
     expect(say(0)).to.be.equal('0');
   });
 
-  it(`Given 1, 26, 334 should say respectively '1', '26', '334'`, () => {
+  it(`Given 1 should say respectively '1'`, () => {
     expect(say(1)).to.be.equal('1');
-    expect(say(26)).to.be.equal('26');
-    expect(say(334)).to.be.equal('334');
   });
 
-  it(`Given 3, 126, 963 should say 'Fizz'`, () => {
+  it(`Given 3 should say 'Fizz'`, () => {
     expect(say(3)).to.be.equal('Fizz');
-    expect(say(126)).to.be.equal('Fizz');
-    expect(say(963)).to.be.equal('Fizz');
   });
 
-  it(`Given 5, 245, 700 should say 'Buzz'`, () => {
+  it(`Given 5 should say 'Buzz'`, () => {
     expect(say(5)).to.be.equal('Buzz');
-    expect(say(245)).to.be.equal('Buzz');
-    expect(say(700)).to.be.equal('Buzz');
   });
 
-  it(`Given 15, 240, 960 should say 'Fizzbuzz'`, () => {
+  it(`Given 15 should say 'Fizzbuzz'`, () => {
     expect(say(15)).to.be.equal('Fizzbuzz');
-    expect(say(240)).to.be.equal('Fizzbuzz');
-    expect(say(960)).to.be.equal('Fizzbuzz');
   });
 });
